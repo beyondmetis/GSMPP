8da3aec4db6fc07a6ba6f0d32db3ecdb2d3b5c62
Todd <tgoodall@utexas.edu>
2015-12-12 12:09:15 -0600

    Removing junk code

--- GSM_DNU_method.py
+++ GSM_DNU_method.py
@@ -15,66 +15,6 @@ from scipy.linalg.blas import dgemm
 
 from sklearn import svm, datasets, ensemble
 
-def gauss_window(lw, sigma):
-    sd = float(sigma)
-    lw = int(lw)
-    weights = [0.0] * (2 * lw + 1)
-    weights[lw] = 1.0
-    sum = 1.0
-    sd *= sd
-    for ii in range(1, lw + 1):
-        tmp = np.exp(-0.5 * float(ii * ii) / sd)
-        weights[lw + ii] = tmp
-        weights[lw - ii] = tmp
-        sum += 2.0 * tmp
-    for ii in range(2 * lw + 1):
-        weights[ii] /= sum
-    return weights
-avg_window = gauss_window(3, 7.0/6.0)
-
-extend_mode = 'constant'#'nearest'#'wrap'
-def SSIM(img1, img2):
-    avg_window = gauss_window(5, 1.5)
-    K_1 = 0.01;				
-    K_2 = 0.03;
-    L = 1;
-
-    C1 = (K_1*L)**2;
-    C2 = (K_2*L)**2;
-#
-    h, w = np.shape(img1)
-    mu1 = np.zeros((h, w))
-    mu2 = np.zeros((h, w))
-    scipy.ndimage.correlate1d(img1, avg_window, 0, mu1, mode=extend_mode) 
-    scipy.ndimage.correlate1d(mu1, avg_window, 1, mu1, mode=extend_mode) 
-    scipy.ndimage.correlate1d(img2, avg_window, 0, mu2, mode=extend_mode) 
-    scipy.ndimage.correlate1d(mu2, avg_window, 1, mu2, mode=extend_mode) 
-    
-    mu1_sq = mu1**2;
-    mu2_sq = mu2**2;
-    mu1_mu2 = mu1*mu2;
-
-    var1 = np.zeros((h, w))
-    var2 = np.zeros((h, w))
-    var12 = np.zeros((h, w))
-
-    scipy.ndimage.correlate1d(img1**2, avg_window, 0, var1, mode=extend_mode) 
-    scipy.ndimage.correlate1d(var1, avg_window, 1, var1, mode=extend_mode) 
-    scipy.ndimage.correlate1d(img2**2, avg_window, 0, var2, mode=extend_mode) 
-    scipy.ndimage.correlate1d(var2, avg_window, 1, var2, mode=extend_mode) 
-    scipy.ndimage.correlate1d(img1*img2, avg_window, 0, var12, mode=extend_mode) 
-    scipy.ndimage.correlate1d(var12, avg_window, 1, var12, mode=extend_mode) 
-
-    sigma1_sq = var1 - mu1_sq;
-    sigma2_sq = var2 - mu2_sq;
-    sigma12 = var12 - mu1_mu2;
-
-    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12+C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
-
-    ssim_map = ssim_map[5:-5, 5:-5]
-
-    return np.mean(ssim_map)
-
 #generate noise 
 def noise_covariance(pyr_lvl, orientation, noise_level, cov_mt, t):
     tstr = ''
